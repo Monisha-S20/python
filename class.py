01d20c20ef203ce3dae8b1a19e0fbd368d0cce1e
Monisha-S20 <72863466+Monisha-S20@users.noreply.github.com>
2025-03-03 01:21:47 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,23 @@
+class Dog:
+   species="canie" #class attribute
+   def __init__(self,name,age):
+      self.name=name      #instance attribute
+      self.age=age        #instance attribute
+ # object creation   
+object = Dog("Buddy",5)
+print(object.name)
+print(object.age)
+print(object.species)
+
+# class Dog:
+#     species = "Canine"  # Class attribute
+
+#     def __init__(self, name, age):
+#         self.name = name  # Instance attribute
+#         self.age = age  # Instance attribute
+
+# # Creating an object of the Dog class
+# dog1 = Dog("Buddy", 3)
+
+# print(dog1.name) 
+# print(dog1.species)
